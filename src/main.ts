511d3cdd2b7ef150b1540152199b0bd205a047ef
LucasNav6 <lucas.navarro@lilab.pe>
2024-11-15 11:27:31 -0300

    feat: catch custom exception

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Exceptions } from './proxess.domain/Exceptions/Exceptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
   app.enableCors({
     origin: ['http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
+
+  // Enable Exceptions
+  app.useGlobalFilters(new Exceptions());
+
+  // Start the server
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

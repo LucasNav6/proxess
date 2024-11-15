3c0fbab7123dad28f1086dedeb7c636b2001abb2
LucasNav6 <lucas.navarro@lilab.pe>
2024-11-15 15:02:09 -0300

    feat: add barrel to createAccount context

---
+++ src/proxess.api/_index.ts
@@ -0,0 +1,4 @@
+import { CreateAccountController } from './auth/create-account/createAccount.controller';
+import { CreateAccountService } from './auth/create-account/createAccount.service';
+
+export { CreateAccountController, CreateAccountService };
